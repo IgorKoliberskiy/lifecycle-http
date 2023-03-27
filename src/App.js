4cf9cc5e54b326028391f42dd1e42a9b04a5c7a4
Igor Koliberskiy <kolibry60rus@mail.ru>
2023-03-27 23:19:16 +0300

    HW lifecycle-http

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './css/style.css';
+import Form from './components/Form';
+import Clocks from './components/Clocks';
+import {nanoid} from "nanoid";
 
 function App() {
+  const [clocks, setClocks] = useState([]);
+  const [form, setForm] = useState({
+    name: '',
+    timeZone: ''
+  })
+  const handleChange = (evt) => {
+    const name = evt.target.name;
+    const value = evt.target.value;
+    setForm((prevForm) => ({...prevForm, [name]: value}));
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (!form.name || !form.timeZone) {
+      console.log('Заполните все поля');
+      return;
+    } 
+
+    const clock = {
+      name: `${form.name}`,
+      offset: Number(`${form.timeZone}`),
+      id: `${nanoid(8)}`
+    }
+
+    setForm(({
+      name: '',
+      timeZone: ''
+    }))
+
+    setClocks(prevClocks => [
+      ...prevClocks, clock
+    ])
+  }  
+  
+  const handleDelete = (evt) => {
+    const newClocks = clocks.filter(el => el.id !== evt.target.parentNode.id);
+    
+    setClocks(newClocks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Form
+        form={form} 
+        onHandleChange={handleChange} 
+        onHandleSubmit={handleSubmit} 
+      ></Form>
+      <Clocks
+        form={form} 
+        clocks={clocks} 
+        onHandleDelete={handleDelete}
+      ></Clocks>
     </div>
   );
 }
